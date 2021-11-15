0aa8a1656ecf4571c94446840b9c565946f4946f
Alexandre Moreau <amoreau@coveo.com>
2021-11-15 16:15:51 -0500

    changing port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const movies = require("./moviesshort.json");
 const detailedmovies = require("./moviesdetailed.json");
 const directors = require("./directors.json");
 const app = express();
-const port = 80;
+const port = 3000;
 const responseSizeLimit = 20;
 
 
