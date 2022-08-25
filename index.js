e2c2a7046e1ff97a886ef55de9c5728987f86e7f
Alexandre Moreau <amoreau@coveo.com>
2022-08-25 13:26:38 -0400

    add updated content

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 const express = require("express");
 const movies = require("./moviesshort.json");
+const moviesfr = require("./moviesfr.json");
 const detailedmovies = require("./moviesdetailed.json");
+const pokemon = require("./pokemon.json");
+const pokemonfr = require("./pokemonfr.json");
+const pokemonitems = require("./pokemonitems.json");
+const path = require("path");
 const directors = require("./directors.json");
+const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 3000;
 const responseSizeLimit = 20;
 
+const docs = new Promise((resolve, reject) => {
+  fs.readFile("./index.html", "utf-8", function (err, data) {
+    if (err) {
+      console.log("Can't read file");
+      reject(err);
+    } else {
+      console.log("Read Docs");
+      resolve(data);
+    }
+  })
+})
+
+const config = new Promise((resolve, reject) => {
+  fs.readFile("./config.html", "utf-8", function (err, data) {
+    if (err) {
+      console.log("Can't read file", "IMDb");
+      reject(err);
+    } else {
+      console.log("Read IMDb Config");
+      resolve(data);
+    }
+  })
+})
+
+const pokconfig = new Promise((resolve, reject) => {
+  fs.readFile("./pokemonconfig.html", "utf-8", function (err, data) {
+    if (err) {
+      console.log("Can't read file", "Pokemon");
+      reject(err);
+    } else {
+      console.log("Read Pokemon Config");
+      resolve(data);
+    }
+  })
+})
 
 function parseStringToInt(str) {
   const parsed = parseInt(str);
@@ -15,6 +56,39 @@ function parseStringToInt(str) {
     return "NaN";
   }
 }
+
+app.get('/', (req, res) => {
+  docs.then(
+    doc => {
+      res.send(doc);
+    }
+  )
+})
+
+
+app.use("/power-up/src", express.static(__dirname + '/moviedemo/src'));
+
+app.get('/power-up/demo/', (req, res) => {
+  res.sendFile(path.join(__dirname + "/moviedemo/", "/index.html"));
+})
+
+app.use('/web', express.static(path.join(__dirname + "/web/")))
+
+app.get('/power-up/config/codeless', (req, res) => {
+  config.then(
+    qconfig => {
+      res.send(qconfig);
+    }
+  )
+})
+
+app.get('/power-up/config/pokemon/codeless', (req, res) => {
+  pokconfig.then(
+    pconfig => {
+      res.send(pconfig);
+    }
+  )
+})
 
 app.get('/movies', (req, res) => {
   if (!req.query["per_page"]) {
@@ -32,19 +106,31 @@ app.get('/movies', (req, res) => {
     req.query["page"] = 1;
   }
 
-  const indexToStart = (req.query["page"] -1) * req.query["per_page"];
+  const indexToStart = (req.query["page"] - 1) * req.query["per_page"];
   const indexToStop = req.query["page"] * req.query["per_page"]
-  let response = movies.slice(indexToStart, indexToStop);
-  if (!response.length) {
-    response = [];
+
+  let response = [];
+  if (req.query["lang"] && req.query["lang"] != "fr" && req.query["lang"] != "en") {
+    response = { "error": "Invalid language key. Valid language keys are: ['en', 'fr']"};
+  } else if (req.query["lang"] == "fr") {
+    response = moviesfr.slice(indexToStart, indexToStop);
+    if (!response.length) {
+      response = [];
+    }
+  } else {
+    response = movies.slice(indexToStart, indexToStop);
+    if (!response.length) {
+      response = [];
+    }
   }
+
   res.send(response)
 })
 
 app.get('/movies/:movieid', (req, res) => {
-  let response = detailedmovies.find(movie => movie.imdb_title_id==req.params.movieid);
+  let response = detailedmovies.find(movie => movie.id == req.params.movieid);
   if (response == null) {
-    response = {"error": "Invalid movie id"}
+    response = { "error": "Invalid movie id" }
   }
   res.send(response);
 })
@@ -65,7 +151,7 @@ app.get('/directors', (req, res) => {
     req.query["page"] = 1;
   }
 
-  const indexToStart = (req.query["page"] -1) * req.query["per_page"];
+  const indexToStart = (req.query["page"] - 1) * req.query["per_page"];
   const indexToStop = req.query["page"] * req.query["per_page"]
   let response = directors.slice(indexToStart, indexToStop);
   if (!response.length) {
@@ -78,15 +164,15 @@ app.get('/directors/:directorid', (req, res) => {
   const directorids = req.params.directorid.split(";");
   const directorsToReturn = [];
   directorids.forEach((directorid) => {
-    const director = directors.find(dir => dir.imdb_name_id==directorid);
+    const director = directors.find(dir => dir.id == directorid);
     if (director != undefined) {
       directorsToReturn.push(director);
     }
   })
   if (directorsToReturn.length < 1) {
-    const error = {"error": "Invalid director id"};
+    const error = { "error": "Invalid director id" };
     res.send(error);
-  } else if (req.query["page"] && req.query["page"]!= 1) {
+  } else if (req.query["page"] && req.query["page"] != 1) {
     const empty = [];
     res.send(empty);
   } else {
@@ -94,6 +180,87 @@ app.get('/directors/:directorid', (req, res) => {
   }
 })
 
+app.get('/pokemon', (req, res) => {
+  if (!req.query["per_page"]) {
+    req.query["per_page"] = responseSizeLimit;
+  }
+  const parsedPerPage = parseStringToInt(req.query["per_page"]);
+  if (parsedPerPage < 1 || parsedPerPage > responseSizeLimit || parsedPerPage == "NaN") {
+    req.query["per_page"] = responseSizeLimit;
+  }
+  if (!req.query["page"]) {
+    req.query["page"] = 1;
+  }
+  const parsedPage = parseStringToInt(req.query["page"]);
+  if (parsedPage < 1 || parsedPage == "NaN") {
+    req.query["page"] = 1;
+  }
+
+  const indexToStart = (req.query["page"] - 1) * req.query["per_page"];
+  const indexToStop = req.query["page"] * req.query["per_page"]
+
+  let response = [];
+
+  if (req.query["lang"] && req.query["lang"] != "fr" && req.query["lang"] != "en") {
+    response = { "error": "Invalid language key. Valid language keys are: ['en', 'fr']"};
+  } else if (req.query["lang"] == "fr") {
+    response = pokemonfr.slice(indexToStart, indexToStop);
+    if (!response.length) {
+      response = [];
+    }
+  } else {
+    response = pokemon.slice(indexToStart, indexToStop);
+    if (!response.length) {
+      response = [];
+    }
+  }
+
+  res.send(response)
+})
+
+app.get('/pokemon/:pokemonid', (req, res) => {
+  let response = pokemon.find(poke => poke.number == req.params.pokemonid);
+  if (response == null) {
+    response = { "error": "Invalid pokemon id" }
+  }
+  res.send(response);
+})
+
+app.get('/pokemon-items', (req, res) => {
+  if (!req.query["per_page"]) {
+    req.query["per_page"] = responseSizeLimit;
+  }
+  const parsedPerPage = parseStringToInt(req.query["per_page"]);
+  if (parsedPerPage < 1 || parsedPerPage > responseSizeLimit || parsedPerPage == "NaN") {
+    req.query["per_page"] = responseSizeLimit;
+  }
+  if (!req.query["page"]) {
+    req.query["page"] = 1;
+  }
+  const parsedPage = parseStringToInt(req.query["page"]);
+  if (parsedPage < 1 || parsedPage == "NaN") {
+    req.query["page"] = 1;
+  }
+
+  const indexToStart = (req.query["page"] - 1) * req.query["per_page"];
+  const indexToStop = req.query["page"] * req.query["per_page"]
+
+  let response = pokemonitems.slice(indexToStart, indexToStop);
+  if (!response.length) {
+    response = [];
+  }
+
+  res.send(response)
+})
+
+app.get('/pokemon-items/:itemid', (req, res) => {
+  let response = pokemonitems.find(poke => poke.id == req.params.itemid);
+  if (response == null) {
+    response = { "error": "Invalid pokemon item id" }
+  }
+  res.send(response);
+})
+
 app.listen(port, () => {
-  console.log(`Movie listing available at http://localhost:${port}`)
+  console.log(`Listings available at http://localhost:${port}`)
 })
---
+++ moviedemo/src/script.js
@@ -0,0 +1,35 @@
+const trivia = [
+    "The charcoal drawing in <i>Titanic</i> was drawn by movie director James Cameron himself.",
+    "The first movie to feature a flushing toilet was Alfred Hitchcock's <i>Psycho</i>, in 1960.",
+    "The movie <i>Toy Story 2</i> was accidentally deleted when it was 90% done, and was only saved because the technical director had a copy at home while working remotely.",
+    "The script for <i>Taxi Driver</i> was written in just two weeks.",
+    "The snow we see in <i>The Wizard of Oz</i> is actually asbestos.",
+    "The house in <i>Up</i> is held up by 10,297 balloons, each of which were individually added by animators.",
+    "The sound of the raptors from <i>Jurassic Park</i> are taken from recordings of tortoises mating.",
+    "The famous line “We're gonna need a bigger boat” from <i>Jaws</i> was improvised.",
+    "Due to its small budget and huge success, the most profitable movie of all time is <i>Paranormal Activity</i>.",
+    "When adjusted for inflation, the highest-grossing film of all time is <i>Gone With The Wind</i>, which made over US$3.7M, adjusted for 2020.",
+    "George Lucas's dog inspired two characters from his movies. His dog's name is Indiana (like Indiana Jones), and its size in the front seat of his car inspired the creation of Chewbacca from Star Wars.",
+    "Before 1940, Oscar winners were given in advance to newspapers, before the actual ceremony.",
+    "The lines of cryptic code from <i>The Matrix</i> are symbols from a sushi cookbook.",
+    "The cat from the opening scene of <i>The Godfather</i> was a stray found on the Paramount lot.",
+    "Tom Hanks was not paid for his role in <i>Forrest Gump</i>. Instead, he decided to take a percentage of the profit from the movie, now estimated at around $40M.",
+    "Keanu Reeves performed almost 95% of his fight scenes in <i>John Wick: Chapter 2</i>.",
+    "Because actor Sean Bean was scared of flying, he would often spend two hours climbing a mountain fully dressed as Boromir to shoot scenes for <i>The Lord of the Rings: The Followship of the Ring</i>.",
+    "The only line Legolas ever says to Frodo in The Lord of the Rings trilogy is “and you have my bow”.",
+    "If you add all the scenes taking place in 1912 in the movie <i>Titanic</i>, it adds up to two hours and forty minutes, the exact time it took for the real Titanic to sink.",
+    "Brad Pitt and Edward Norton both learned how to make soap for <i>Fight Club</i>.",
+    "Brandon Fraser almost died when filming <i>The Mummy</i> during the hanging scene, and had to be resuscitated.",
+    "To lighten the mood, Robin Williams told jokes and performed comedy sketches on the set of <i>Schindler's List</i>. Some of his jokes ended up being used for his character in <i>Aladdin</i>.",
+    "Stanley Kubrick destroyed almost all the props and sets from <i>2001: A Space Odyssey</i> because he didn't want them to be used in any lesser science fiction films.",
+    "James Cameron was convinced CGI effects were ready for his next movie, <i>Avatar</i>, when he saw Gollum in <i>The Lord of the Rings: The Two Towers</i>.",
+    "The mask used in <i>Halloween</i> is a Captain Kirk face mask spray-painted white, with teased out hair, and with reshaped eye holes.",
+    "The <a href=\"https://www.youtube.com/watch?v=xn6hhrX34Pw\">Wilhelm Scream</a> is a stock sound effect used in over 400 films and TV shows, including all Star Wars and Indiana Jones films, two Lord of the Rings, most Tarantino movies, and many Disney animated classics.",
+    "In order to subconsciously destabilize the audience, the movie <i>Seven</i> shot all the office scenes in houses refurnished as offices and all the home scenes in offices refurbished as living spaces."
+]
+
+const triviaToShow = trivia[Math.floor(Math.random() * trivia.length)];
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#triviaquote").innerHTML = triviaToShow;
+});
